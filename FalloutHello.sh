d33a049308f443431feaf86668d59923361863da
Nikolai Zhirkov <zhirkov_lol@mail.ru>
2023-11-13 15:23:21 +0300

    init

---
+++ FalloutHello.sh
@@ -0,0 +1,55 @@
+## THIS IS SCRIPT FOR START UBUNTU TERMINAL
+# Script Maker Nikola3000 Z
+# install nano ~/.bashrc -> after first commit -> bash ~/FalloutHello.sh
+echo "===================================== RobcOS ==================================="
+echo "WELCOME TO ROBCO INDUSTRIES (TM) TERMINAL"
+sleep 0.8
+echo ">>ADMIN LOGIN"
+echo "RTT-V3000"
+echo "Initializing Robco Industries(TM) HF Boot Agent v2.3.11"
+for i in 1 2 3
+do
+echo -ne '.\r'
+sleep 1
+echo -ne '..\r'
+sleep 1
+echo -ne '...\r'
+sleep 1
+echo -ne '    \r'
+done
+sleep 1
+echo -ne ">>SUCCESS\n"
+sleep 0.5
+echo "RETROS BIOS"
+echo "------------------------------------------------------------------------------"
+sleep 1
+echo "RBIOS 4.02.08.00 52EE5.ES7.F8.SD23"
+echo "Copyright 2201 2203 Robco Ind."
+sleep 0.5
+echo "Root (SAX>home>nikolai)"
+sleep 0.5
+echo "Maintenance MODE ON v.2.1.03"
+echo "=============================================================================="
+echo ">PRESS ENTER TO CONTINUE"
+read continue
+clear
+sleep 0.7
+echo "                          ROBCO INDUSTRIES UNIFIED OPERATING SYSTEM"
+sleep 0.2
+echo "                            COPYRIGHT 2075-2077 ROBCO INDUSTRIES"
+sleep 1
+echo "                                        -Server 6- "
+sleep 1
+echo "                  'SoftLock Solution'"
+sleep 0.7
+echo "                  'CyberIT AED RU'"
+sleep 0.7
+echo "                  'Your safety - Us work'"
+sleep 0.5
+echo "                                                        (c)2076 Robco Ind."
+sleep 0.8
+echo "-------------------------------------------------------------------------------"
+echo " - RobcOS v.85.1.2- "
+echo "==============================================================================="
+
+
